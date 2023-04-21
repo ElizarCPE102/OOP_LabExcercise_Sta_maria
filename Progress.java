b2aa6042034de6fab0cd6f6f1b79fc88a54fe210
ElizarCPE102 <125979254+ElizarCPE102@users.noreply.github.com>
2023-04-21 06:46:38 -0700

    Update Progress.java

--- Progress.java
+++ Progress.java
@@ -14,57 +14,82 @@ import java.awt.Graphics;
 
 
 public class Progress extends JFrame implements ActionListener {
-    private int energy;//energy of the pet
-    private int health;//health of the pet
-    private double level;//level of the pet
-    private int hatchrate;//when hatching the egg which will turn into a pet
+    protected int energy;//energy of the pet
+    protected int health;//health of the pet
+    protected int level;//level of the pet
+    protected String petname;//name of the pet
+    private String status;
+    private int hatchrate;// egg hatch timer
+
+
     private Scanner input;
     private JFrame f;
     private JLabel kk;
-    private Image dbImage;
-    Image im;
     
+    public Progress(String petname,int energy,int health,int level){
+        this.energy = energy;
+        this.health = health;
+        this.level = level;
+        this.petname = petname;
+       }
+       
 
+    Image im;
     int x, y;
     final TextField tf = new TextField();
+    //buttons
     Button start = new Button(" Start ");
+    Button newG = new Button("New Game");
+    //fonts of front page
+    Font fnt = new Font ("Times New Roman", Font.ITALIC,150);
+    Font fnt1 = new Font ("Times New Roman", Font.LAYOUT_NO_LIMIT_CONTEXT,30);
 
-    Progress(){
+    Font startfnt = new Font ("Times New Roman", Font.BOLD,150);
+
+    
+    
+    Progress(){  
         ImageIcon j = new ImageIcon("C:/Elizar/Game Project/background1.png");
         im = j.getImage();
+        im.getScaledInstance(2000, 2000, Image.SCALE_DEFAULT);
         
-       
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);      
         setTitle("Pet Z");
         setSize(2000, 1000);
         setLayout(null);
         setVisible(true);
-        setResizable(false);
-
-        
+        setResizable(true);
+    
         start.setBackground(Color.MAGENTA);
         start.setBounds(520,300,200,40);
         start.addActionListener(this);
         add(start); 
-        
 
+       
     }
     public void paint(Graphics g){
+        Graphics2D g2 = (Graphics2D) g;
+        
         g.drawImage(im, 0, 0, null);
+        g2.setFont(fnt);
+        g.drawString("PET Z ", 435, 200);
+        g2.setFont(fnt1);
+        g.drawString("The Legend has risen ", 435, 240);
+        g2.setBackground(Color.CYAN);
+        
+
+
     }
     
     public void actionPerformed(ActionEvent e){
-        start.setBackground(Color.ORANGE);
-        tf.setBounds(540,500, 150,20);  
-        add(tf);  
-        tf.setVisible(true);
-        tf.setText("Enter pet Name");
-        tf.setBackground(Color.ORANGE);    
+   
     }
+
 
 
     public static void main(String args[]){
         Progress f = new Progress();
+        
         
     }
 }
