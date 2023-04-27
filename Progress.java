58ce0dd5b9b4347240995686d02a62ffe5cfdbc0
ElizarCPE102 <125979254+ElizarCPE102@users.noreply.github.com>
2023-04-27 08:32:02 -0700

    Update Progress.java

--- Progress.java
+++ Progress.java
@@ -1,21 +1,16 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-package progress;
+package Progress;
 
 /*
 Elizar Sta Maria
 Jucel Anthonny
 CPE - 102
 NetBeans
-
-
 */
 
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.JFrame;
+import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -25,106 +20,80 @@ import java.awt.Graphics;
 
 
 public class Progress extends JFrame implements ActionListener {
-    protected int energy;//energy of the pet
-    protected int health;//health of the pet
-    protected int level;//level of the pet
-    protected String petname;//name of the pet
-    private String status;
-    private int hatchrate;// egg hatch timer
-    private Scanner input;//for the name of the pet,
-
-    
-    public Progress(String petname,int energy,int health,int level){
-        this.energy = energy;
-        this.health = health;
-        this.level = level;
-        this.petname = petname;
-       }
-    
-      int x, y;
-      
+    private Scanner input;//for the name of the pet,  
     //images
     Image im;//background frame 1
     Image im1;// background frame 2
-    ImageIcon j = new ImageIcon("C:/SampleBG/background.png"); 
-    ImageIcon j1 = new ImageIcon("C:/SampleBG/background3.png");
-    
-  
-    
+    Image gif;//egg icon
+
+    //get images from sources
+    ImageIcon j = new ImageIcon("C:\\Elizar\\Game Project\\clod.png"); 
+    ImageIcon j1 = new ImageIcon("C:\\Elizar\\Game Project\\background2.png");
+    ImageIcon g = new ImageIcon("C:\\Elizar\\Game Project\\Egg2.png");
     
     //buttons
-    JButton start = new JButton(new ImageIcon("C:/SampleBG/strt.png"));
+    JButton start = new JButton(new ImageIcon("C:\\Elizar\\Game Project\\startbtn.png"));
     final TextField tf = new TextField("Enter Name");
     Button newG = new Button();
     
-    
     //font of main frame
-    Font fnt = new Font ("Times New Roman", Font.ITALIC,500);
-    Font fnt1 = new Font ("Times New Roman", Font.LAYOUT_NO_LIMIT_CONTEXT,30);
-    
-    
-  
+    Font fnt = new Font ("Times New Roman", Font.BOLD,100);
     
     // 2nd frame
     public void actionPerformed(ActionEvent e){
       dispose();
-
-      JPanel k = new JPanel();
-      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      setSize(490, 800);
-      setBackground(Color.yellow);
-      setLocationRelativeTo(null);
-      setLayout(null);
-      setResizable(false);
-      
       tf.setBounds(145,500,200,30);
-      add(tf);
+      add(tf);//add textfield button in 2nd frame
+      im1 = j1.getImage();//load background image of another frame
+      gif = g.getImage();//get gif egg animation
+      start.setVisible(false);//prevent start button to appear in 2nd frame
       setVisible(true);
-      
-      im1 = j1.getImage();
- 
-      
+
+
     }
     
     //Load Image, Frame and Buttons.
     Progress(){  
         dispose();
-        im = j.getImage();
-        JFrame frame = new JFrame();
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);      
+        im = j.getImage();// load background in the main frame
+       
+        start.setBounds(90,420,270,80);
+        // clicking this button moves to the next frame to 2nd frame
+        start.addActionListener(this);
+        add(start);
+
         setTitle("Pet Z");
         setSize(490, 800);
         setLayout(null);
         setVisible(true);
-        setResizable(false);
+        setResizable(true);
 
-        start.setBounds(90,420,290,100);
-        start.addActionListener(this);
-        add(start);
-  
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
     
     //insert Images and fonts to be loaded
     public void paint(Graphics g){
-        Graphics2D g2 = (Graphics2D) g;    
+        Graphics g2 = (Graphics) g;    
         Graphics g3 = (Graphics) g; 
+        Graphics g4 = (Graphics) g;
+        Graphics g5 = (Graphics) g;
+        Graphics g6 = (Graphics) g;
         g2.drawImage(im, 0, 0, null);
         g3.drawImage(im1, 0, 0, null);
-        g.setFont(fnt);
-        g.setFont(fnt1);
-        g.drawString("PET Z ", 60, 240);
-        g.drawString("The Legend has risen ", 60, 280);
+        g4.setFont(fnt);
+      //  g5.setFont(fnt1);
+        g4.drawString("PET Z ", 60, 240);
+        g5.drawString("The Legend has risen ", 60, 280);
+        
+        g6.drawImage(gif, 0, 0, this);
+
         
 
     }
-    
-    
-  
     public static void main(String[] args) {
         Progress k = new Progress();
-        
-        
-        
+       
     }
     
 }
+
