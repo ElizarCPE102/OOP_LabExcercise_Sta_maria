037847b658c860fbe1d743ffaeae3faae4977949
ElizarCPE102 <125979254+ElizarCPE102@users.noreply.github.com>
2023-04-29 14:12:22 +0800

    Update TaskPerf6.java

--- TaskPerf6.java
+++ TaskPerf6.java
@@ -10,40 +10,40 @@ import static java.nio.file.StandardOpenOption.*;
 import java.util.*;
 
 public class TaskPerf6 {
+    
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        Path doc = Paths.get("C:\\Elizar\\record3.txt");
-        String delimiter = ",";
-        String account = "";
+        Path filepath = Paths.get("C:\\Sta_Maria_Elizar\\Records.txt");
+        String account = "Acc: ";
  
+        for (int x = 1 ;  x > 0; x++){
         try {
-            BufferedWriter writer = Files.newBufferedWriter(doc, CREATE, APPEND);
+            BufferedWriter writer = Files.newBufferedWriter(filepath, CREATE, APPEND);
             System.out.println("===============");
             System.out.println("1. Register \n" + "2. Login ");
             System.out.println("===============");
 
             int select = scan.nextInt();
-            switch (select) {
-                case 1:
+            if(select == 1){
                     System.out.println("Enter new username: ");
                     String name = scan.next();
                     System.out.println("Enter new password: ");
                     String password = scan.next();
-                    account = name + delimiter + password;
+                    account = name + "," + password;
                     writer.write(account);
                     writer.newLine();
                     System.out.println("Registered successfully");
-                    writer.close();
-                    break;
-                case 2:
+                    writer.close();            
+            }
+            if(select == 2){
                     System.out.println("Enter username: ");
                     String kname = scan.next();
                     System.out.println("Enter password: ");
                     String kpass = scan.next();
-                    Scanner checker = new Scanner(doc);
+                    Scanner checker = new Scanner(filepath);
                     while (checker.hasNextLine()) {
                         String line = checker.nextLine();
-                        String[] acc = line.split(delimiter);
+                        String[] acc = line.split(",");
                         if (kname.equals(acc[0]) && kpass.equals(acc[1])) {
                             System.out.println("Successfully logged In!");
                             break;
@@ -53,11 +53,12 @@ public class TaskPerf6 {
                         }
                     }
                     checker.close();
-                    break;
             }
         } catch (Exception e) {
-            
+            System.out.println(e.getMessage());
         }
+        continue;
+    }
         scan.close();
     }
 
